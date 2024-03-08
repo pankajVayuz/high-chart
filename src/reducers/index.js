41b4aa23a07ec895ab6e99c1b24b3161d9151b2d
Pankaj kumar <pankaj.kumar@vayuz.com>
2024-03-08 13:38:37 +0530

    score board data bind with redux state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@
 import { combineReducers } from 'redux';
 
 import userReducer from '../reducers/slices/userSlice';
+import partyData from '../reducers/slices/scoreBoardSlice';
 
 
 const rootReducer = combineReducers({
 
-  user:userReducer
+  user:userReducer,
+  partyData:partyData
   // Add other reducers if any
 });
 
