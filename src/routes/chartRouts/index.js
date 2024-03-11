93c7975946bf6cf1c8b9a77f2cec05a0057d6d40
Pankaj kumar <pankaj.kumar@vayuz.com>
2024-03-11 12:11:38 +0530

    code refactor

--- src/routes/chartRouts/index.js
+++ src/routes/chartRouts/index.js
@@ -1,13 +1,13 @@
 import LazyLoadingHOC from "../../components/hocComponents/LazyLoadingHOC";
 
-const Chart1 = LazyLoadingHOC(() => import("../../page/charts/keyIndicate/KeyIndices"));
-const Chart2 = LazyLoadingHOC(() => import("../../page/charts/columnChart"));
-const Chart3 = LazyLoadingHOC(() => import("../../page/charts/maxPainChart"));
-const Chart4 = LazyLoadingHOC(() => import("../../page/charts/donutChart"));
-const Chart5 = LazyLoadingHOC(() => import("../../page/charts/intradayStockChart"));
-const Chart6 = LazyLoadingHOC(() => import("../../page/charts/lineChart"));
-const Chart7 = LazyLoadingHOC(() => import("../../page/charts/stockChart"));
-const Chart8 = LazyLoadingHOC(() => import("../../page/charts/solarEmploymentChart"));
+const Chart1 = LazyLoadingHOC(() => import("../../page/charts/chart1/KeyIndices"));
+const Chart2 = LazyLoadingHOC(() => import("../../page/charts/chart2"));
+const Chart3 = LazyLoadingHOC(() => import("../../page/charts/chart3"));
+const Chart4 = LazyLoadingHOC(() => import("../../page/charts/chart4"));
+const Chart5 = LazyLoadingHOC(() => import("../../page/charts/chart5"));
+const Chart6 = LazyLoadingHOC(() => import("../../page/charts/chart6"));
+const Chart7 = LazyLoadingHOC(() => import("../../page/charts/chart7"));
+const Chart8 = LazyLoadingHOC(() => import("../../page/charts/chart8"));
 const Chart9 = LazyLoadingHOC(() => import("../../page/charts/livescoreboard"));
 
 const chartRoutes = [
